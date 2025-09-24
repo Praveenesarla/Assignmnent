a9d32f601bef1979fd2f8a284cbf20e2ab79b0c2
Praveenesarla <esarlapraveen16@gmail.com>
2025-09-24 17:05:38 +0530

    added loader in webview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { ActivityIndicator, Button, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 import {
@@ -9,10 +9,18 @@ import {
 
 export default function WebViewScreen() {
   const [notified, setNotified] = useState(false);
+  const [loading, setLoading] = useState(true);
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      {loading && (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
       <WebView
+        onLoadStart={() => setLoading(true)}
         onLoadEnd={() => {
+          setLoading(false);
           if (!notified) {
             triggerLocalNotification(
               "WebView Loaded!",
@@ -56,5 +64,10 @@ const styles = StyleSheet.create({
   buttons: {
     padding: 10,
     gap: 10,
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
